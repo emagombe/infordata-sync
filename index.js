b1ac58c5a167ac86c1cb3956db66c7f1aed94bfa
Edson Magombe <edson.patricio.39@gmail.com>
2020-02-06 09:11:48 +0200

    update

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ const service = require('./service.js');
 const { ipcMain } = require('electron');
 
 app.on('ready', () => {
+	service();
 	// Create the browser window.
-	let win = new BrowserWindow({
-		center: true,
-		width: 800,
-		height: 600,
-		frame: false,
-		resizable: false,
-	    webPreferences: {
-	    	nodeIntegration: true,
-	    	devTools: true,
-	    }
-	});
-	// and load the index.html of the app.
-	win.loadFile('./windows/index.html');
+	// let win = new BrowserWindow({
+	// 	center: true,
+	// 	width: 800,
+	// 	height: 600,
+	// 	frame: false,
+	// 	resizable: false,
+	//     webPreferences: {
+	//     	nodeIntegration: true,
+	//     	devTools: true,
+	//     }
+	// });
+	// // and load the index.html of the app.
+	// win.loadFile('./windows/index.html');
 });
